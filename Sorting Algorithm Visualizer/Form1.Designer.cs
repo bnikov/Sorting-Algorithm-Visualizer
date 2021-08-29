be260ef3c663c12f53309ecc82a63fc22930c8b6
bojan <nikov.bojan@gmail.com>
2021-08-29 21:11:45 +0200

    Add project files.

---
+++ Sorting Algorithm Visualizer/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Sorting_Algorithm_Visualizer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.tbSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbAlgorithm = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.nudDelay = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDelay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbSize
+            // 
+            this.tbSize.Location = new System.Drawing.Point(16, 31);
+            this.tbSize.Name = "tbSize";
+            this.tbSize.Size = new System.Drawing.Size(132, 20);
+            this.tbSize.TabIndex = 0;
+            this.tbSize.Validating += new System.ComponentModel.CancelEventHandler(this.tbSize_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Array Elements ";
+            // 
+            // cbAlgorithm
+            // 
+            this.cbAlgorithm.FormattingEnabled = true;
+            this.cbAlgorithm.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "QuickSort",
+            "Selection Sort",
+            "Insertion Sort",
+            "Merge Sort",
+            "Radix Sort",
+            "Shell Sort",
+            "Cocktail Sort"});
+            this.cbAlgorithm.Location = new System.Drawing.Point(19, 79);
+            this.cbAlgorithm.Name = "cbAlgorithm";
+            this.cbAlgorithm.Size = new System.Drawing.Size(132, 21);
+            this.cbAlgorithm.TabIndex = 2;
+            this.cbAlgorithm.Text = "Bubble Sort";
+            this.cbAlgorithm.Validating += new System.ComponentModel.CancelEventHandler(this.cbAlgorithm_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(208, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(611, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sorting Algorithm Visualizer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(16, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sorting Algorithm";
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(20, 166);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(132, 23);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Start Sorting";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // nudDelay
+            // 
+            this.nudDelay.Location = new System.Drawing.Point(19, 127);
+            this.nudDelay.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudDelay.Name = "nudDelay";
+            this.nudDelay.Size = new System.Drawing.Size(129, 20);
+            this.nudDelay.TabIndex = 6;
+            this.nudDelay.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudDelay.ValueChanged += new System.EventHandler(this.nudDelay_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(16, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Delay Between Swaps (ms)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(309, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(378, 58);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "by:\r\nBojan Nikov and Luka Cheshlarov \r\n";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(944, 681);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudDelay);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbAlgorithm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDelay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbAlgorithm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.NumericUpDown nudDelay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Sorting Algorithm Visualizer/Form1.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Sorting_Algorithm_Visualizer {
+    public partial class Form1 : Form {
+
+        public int[] arr { get; set; }
+
+        public int size { get; set; }
+
+        public int delay { get; set; }
+
+        Color c;
+
+        public Form1() {
+            InitializeComponent();
+            DoubleBuffered = true;
+            c = Color.White;
+        }
+
+
+        // validating array size textbox  
+        private void tbSize_Validating(object sender, CancelEventArgs e) {
+            bool hasLetters = false;
+            foreach (char c in tbSize.Text) {
+                if (!char.IsDigit(c)) {
+                    hasLetters = true;
+                }
+            }
+            if (hasLetters || tbSize.Text.Length == 0 || int.Parse(tbSize.Text) > 1000) {
+                errorProvider1.SetError(tbSize, "Enter a number (Max 1000)");
+                e.Cancel = true;
+            }
+            else {
+                errorProvider1.SetError(tbSize, null);
+                e.Cancel = false;
+            }
+        }
+
+        // validating sorting algorithm dropdown
+        private void cbAlgorithm_Validating(object sender, CancelEventArgs e) {
+            string input = cbAlgorithm.Text;
+            bool valid = false;
+            foreach (string s in cbAlgorithm.Items) {
+                if (input == s) {
+                    valid = true;
+                }
+            }
+            if (valid) {
+                errorProvider1.SetError(cbAlgorithm, null);
+                e.Cancel = false;
+            }
+            else {
+                errorProvider1.SetError(cbAlgorithm, "Select a sorting algorithm from the dropdown");
+                e.Cancel = true;
+            }
+        }
+
+
+        // start sorting button
+        private void btnSort_Click(object sender, EventArgs e) {
+            btnSort.Enabled = false;
+            Random rand = new Random();
+            c = Color.White;
+            arr = new int[int.Parse(tbSize.Text)];
+            size = int.Parse(tbSize.Text);
+            delay = (int)nudDelay.Value;
+
+            if (size > 900) {
+                this.Width = 1000;
+            }
+
+            // creating array using the size as a range
+            for (int i = 0; i < size; i++) {
+                arr[i] = i + 1;
+            }
+
+            // shuffling the array
+            for (int i = 0; i < size; i++) {
+                int j = rand.Next(size);
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+
+            // checking which algorithm to use
+            string selectedAlgorithm = cbAlgorithm.SelectedItem.ToString();
+
+            if (selectedAlgorithm == "Bubble Sort") {
+                bubbleSort();
+            }
+
+            if (selectedAlgorithm == "QuickSort") {
+                quickSort();
+            }
+
+            if (selectedAlgorithm == "Selection Sort") {
+                selectionSort();
+            }
+
+            if (selectedAlgorithm == "Insertion Sort") {
+                insertionSort();
+            }
+
+            if (selectedAlgorithm == "Merge Sort") {
+                mergeSort();
+            }
+
+            if (selectedAlgorithm == "Radix Sort") {
+                radixSort();
+            }
+
+            if (selectedAlgorithm == "Shell Sort") {
+                shellSort();
+            }
+
+            if (selectedAlgorithm == "Cocktail Sort") {
+                cocktailSort();
+            }
+            Invalidate();
+        }
+
+
+        // drawing the array in bar graph form
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+
+            Brush b = new SolidBrush(c);
+            int gap = 2;
+            if (size > 300) {
+                gap = 0;
+            }
+
+            for (int i = 0; i < size; i++) {
+                int x = i * (this.Width - 10) / size;
+                int height = (int)(this.Height * 0.6) * arr[i] / size + 20;
+                int y = this.Height - 40 - height;
+                int width = this.Width / size - gap;
+                e.Graphics.FillRectangle(b, new Rectangle(x, y, width, height));
+                if (size <= 30) {
+                    e.Graphics.DrawString(arr[i].ToString(), new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Point), Brushes.Black, x + width / 2 - 15, this.Height - 70);
+                }
+            }
+            b.Dispose();
+        }
+
+
+        // change delay
+        private void nudDelay_ValueChanged(object sender, EventArgs e) {
+            delay = (int)nudDelay.Value;
+        }
+
+
+        // ============= sorting algorithms ============= 
+
+
+        // Bubble Sort
+        public async void bubbleSort() {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        int tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                        Invalidate();
+                        await Task.Delay(delay);
+                    }
+                }
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+        // QuickSort
+        public async void quickSort() {
+
+            await quickSort_(arr, 0, arr.Length - 1);
+
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+
+            void swap(int[] arr, int i, int j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                Console.WriteLine("a");
+
+            }
+
+            async Task quickSort_(int[] arr, int low, int high) {
+                Invalidate();
+                await Task.Delay(delay);
+                if (low < high) {
+                    int pivot = arr[high];
+                    int i = (low - 1);
+
+                    for (int j = low; j <= high - 1; j++) {
+                        if (arr[j] < pivot) {
+                            i++;
+                            swap(arr, i, j);
+                            Invalidate();
+                            await Task.Delay(delay);
+                        }
+                    }
+                    swap(arr, i + 1, high);
+                    int pi = (i + 1);
+                    await quickSort_(arr, low, pi - 1);
+                    await quickSort_(arr, pi + 1, high);
+                }
+            }
+        }
+
+
+        // Selection Sort
+        public async void selectionSort() {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++) {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                }
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+                Invalidate();
+                await Task.Delay(delay);
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+
+        // Insertion Sort
+        public async void insertionSort() {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i) {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                    Invalidate();
+                    await Task.Delay(delay);
+                }
+                arr[j + 1] = key;
+                Invalidate();
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+        
+        // Merge Sort
+        async public void mergeSort() {
+
+            await SortMethod(arr, 0, arr.Length - 1);
+            c = Color.Green;
+            Invalidate();
+
+            async Task MergeMethod(int[] arr, int left, int mid, int right) {
+                Invalidate();
+                await Task.Delay(delay);
+                int[] temp = new int[arr.Length];
+                int i, left_end, num_elements, tmp_pos;
+                left_end = (mid - 1);
+                tmp_pos = left;
+                num_elements = (right - left + 1);
+
+                while ((left <= left_end) && (mid <= right)) {
+                    if (arr[left] <= arr[mid]) {
+                        temp[tmp_pos++] = arr[left++];
+                    }
+                    else {
+                        temp[tmp_pos++] = arr[mid++];
+                    }
+
+                }
+                while (left <= left_end) {
+                    temp[tmp_pos++] = arr[left++];
+                }
+
+                while (mid <= right) {
+                    temp[tmp_pos++] = arr[mid++];
+                }
+
+                for (i = 0; i < num_elements; i++) {
+                    Invalidate();
+                    await Task.Delay(delay);
+                    arr[right] = temp[right];
+                    right--;
+                }
+            }
+
+            async Task SortMethod(int[] arr, int left, int right) {
+                int mid;
+                if (right > left) {
+                    mid = (right + left) / 2;
+                    await SortMethod(arr, left, mid);
+                    await SortMethod(arr, (mid + 1), right);
+                    await MergeMethod(arr, left, (mid + 1), right);
+                }
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+
+        // Radix Sort
+        public async void radixSort() {
+
+            await radixsort_(arr, arr.Length);
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+
+            int getMax(int[] a, int n) {
+                int mx = a[0];
+                for (int i = 1; i < n; i++) {
+                    if (a[i] > mx) {
+                        mx = a[i];
+                    }
+                }
+                return mx;
+            }
+
+            async Task countSort(int[] arr, int n, int exp) {
+                int[] output = new int[n];
+                int i;
+                int[] count = new int[10];
+
+                for (i = 0; i < 10; i++) {
+                    count[i] = 0;
+                }
+
+                for (i = 0; i < n; i++) {
+                    count[(arr[i] / exp) % 10]++;
+                }
+
+                for (i = 1; i < 10; i++) {
+                    count[i] += count[i - 1];
+                }
+
+                for (i = n - 1; i >= 0; i--) {
+                    output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+                    count[(arr[i] / exp) % 10]--;
+                }
+
+                for (i = 0; i < n; i++) {
+                    arr[i] = output[i];
+                    Invalidate();
+                    await Task.Delay(delay);
+                }
+            }
+
+            async Task radixsort_(int[] arr, int n) {
+                int m = getMax(arr, n);
+
+                for (int exp = 1; m / exp > 0; exp *= 10) {
+                    await countSort(arr, n, exp);
+                }
+            }
+        }
+
+
+        // Shell Sort
+        public async void shellSort() {
+            int n = arr.Length;
+            for (int gap = n / 2; gap > 0; gap /= 2) {
+                for (int i = gap; i < n; i += 1) {
+                    int temp = arr[i];
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                        arr[j] = arr[j - gap];
+                    }
+                    arr[j] = temp;
+                    Invalidate();
+                    await Task.Delay(delay);
+                }
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+
+        // Cocktail Sort
+        public async void cocktailSort() {
+            bool swapped = true;
+            int start = 0;
+            int end = arr.Length;
+
+            while (swapped == true) {
+                swapped = false;
+                for (int i = start; i < end - 1; ++i) {
+                    if (arr[i] > arr[i + 1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                        Invalidate();
+                        await Task.Delay(delay);
+                    }
+                }
+                if (swapped == false) {
+                    break;
+                }
+                swapped = false;
+                end--;
+                for (int i = end - 1; i >= start; i--) {
+                    if (arr[i] > arr[i + 1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                        Invalidate();
+                        await Task.Delay(delay);
+                    }
+                }
+                start++;
+            }
+            c = Color.Green;
+            Invalidate();
+            btnSort.Enabled = true;
+        }
+
+
+    }
+}
